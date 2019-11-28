c8c9f51e849fa1f638f80d7f96bad8fbde3332d7
Saeed Taheri <saeed.taheri@gmail.com>
2019-11-28 13:16:56 +0330

    Gradients are no longer shown in RingView.

--- TeamApp/View/RingView.swift
+++ TeamApp/View/RingView.swift
@@ -9,45 +9,52 @@
 import SwiftUI
 
 struct RingView: View {
-	
-	private let colors = Gradient(colors: [Color.red, Color.yellow, Color.green, Color.red])
+        
+    @Binding var rating: Int
+    @State var ringWidth: CGFloat = 30
 
-	@Binding var rating: Int
-	@State var ringWidth: CGFloat = 30
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(style: StrokeStyle(
+                    lineWidth: ringWidth,
+                    lineCap: .round,
+                    lineJoin: .round))
+                .fill(Color(UIColor.systemGray4))
 
-	var body: some View {
-		ZStack {
-			Circle()
-				.stroke(style: StrokeStyle(
-					lineWidth: ringWidth,
-					lineCap: .round,
-					lineJoin: .round))
-				.fill(Color(UIColor.systemGray4))
+            Circle()
+                .trim(from: 0, to: CGFloat(rating) / 10.0)
+                .stroke(style: StrokeStyle(
+                    lineWidth: ringWidth,
+                    lineCap: .round,
+                    lineJoin: .round))
+                .fill(Color.gray)
+                .blur(radius: round(0.125 * ringWidth))
+                .animation(Animation.default)
+            
+            Circle()
+                .trim(from: 0, to: CGFloat(rating) / 10.0)
+                .stroke(style: StrokeStyle(
+                    lineWidth: ringWidth,
+                    lineCap: .round,
+                    lineJoin: .round))
+                .fill(Color("Color\(rating)"))
+                .animation(Animation.default)
 
-			Circle()
-				.trim(from: 0, to: CGFloat(rating) / 10.0)
-				.stroke(style: StrokeStyle(
-					lineWidth: ringWidth,
-					lineCap: .round,
-					lineJoin: .round))
-				.fill(AngularGradient(gradient: colors, center: .center, startAngle: .degrees(36), endAngle: .degrees(396)))
-				.shadow(radius: round(0.125 * ringWidth))
-				.animation(Animation.default)
-
-			Text("\(rating)")
-				.font(Font.system(size: round(ringWidth * 3 - 8),
-								  weight: .bold,
-								  design: .rounded))
-				.rotationEffect(.degrees(90))
-				.foregroundColor(Color.primary)
-		}
-		.rotationEffect(.degrees(270))
-		.padding(round(ringWidth * 0.8))
-		.frame(width: round(ringWidth * 6.5),
-			   height: round(ringWidth * 6.5))
+            Text("\(rating)")
+                .font(Font.system(size: round(ringWidth * 3 - 8),
+                                  weight: .bold,
+                                  design: .rounded))
+                .rotationEffect(.degrees(90))
+                .foregroundColor(Color.primary)
+        }
+        .rotationEffect(.degrees(270))
+        .padding(round(ringWidth * 0.8))
+        .frame(width: round(ringWidth * 6.5),
+               height: round(ringWidth * 6.5))
 
 
-	}
+    }
 }
 
 struct RingView_Previews: PreviewProvider {
